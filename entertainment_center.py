fd1bac97b76082ef3bed9b48d54cfd5620a02c13
Nick Mandal <nicholas.mandal@gmail.com>
2017-01-06 13:17:37 -0600

    Added function to decrease reduncancy

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,97 +6,28 @@ import requests
 image_url = "https://image.tmdb.org/t/p/w500"
 trailer_url= "https://www.youtube.com/watch?v="
 
-url = "https://api.themoviedb.org/3/movie/321741?api_key=a04cb5827933e112089c0827a6fbf635&append_to_response=videos,releases"
-response = requests.get(url)
-data = json.loads(response.text)
-print(data)
-title = data["original_title"]
-overview = data["overview"]
-image = image_url + data["poster_path"]
-trailer = trailer_url + data["videos"]["results"][0]["key"]
-runtime = data["runtime"]
-genre = data["genres"][0]["name"]
-content_rating = data["releases"]["countries"][0]["certification"]
-rating = data["vote_average"]
-release_date = data["release_date"]
+def create_movie(id):
+  url = "https://api.themoviedb.org/3/movie/%s?api_key=a04cb5827933e112089c0827a6fbf635&append_to_response=videos,releases" % id
+  response = requests.get(url)
+  data = json.loads(response.text)
+  title = data["original_title"]
+  overview = data["overview"]
+  image = image_url + data["poster_path"]
+  trailer = trailer_url + data["videos"]["results"][0]["key"]
+  runtime = data["runtime"]
+  genre = data["genres"][0]["name"]
+  content_rating = data["releases"]["countries"][0]["certification"]
+  rating = data["vote_average"]
+  release_date = data["release_date"]
+  movie = media.Movie(title, overview, image, trailer, runtime, genre, content_rating, rating, release_date)
+  return movie
 
-concussion = media.Movie(title, overview, image, trailer, runtime, genre, content_rating, rating, release_date) 
-
-url = "https://api.themoviedb.org/3/movie/356305?api_key=a04cb5827933e112089c0827a6fbf635&append_to_response=videos,releases"
-response = requests.get(url)
-data = json.loads(response.text)
-title = data["original_title"]
-overview = data["overview"]
-image = image_url + data["poster_path"]
-trailer = trailer_url + data["videos"]["results"][0]["key"]
-runtime = data["runtime"]
-genre = data["genres"][0]["name"]
-content_rating = data["releases"]["countries"][0]["certification"]
-rating = data["vote_average"]
-release_date = data["release_date"]
-
-why_him = media.Movie(title, overview, image, trailer, runtime, genre, content_rating, rating, release_date)
-
-url = "https://api.themoviedb.org/3/movie/297761?api_key=a04cb5827933e112089c0827a6fbf635&append_to_response=videos,releases"
-response = requests.get(url)
-data = json.loads(response.text)
-title = data["original_title"]
-overview = data["overview"]
-image = image_url + data["poster_path"]
-trailer = trailer_url + data["videos"]["results"][0]["key"]
-runtime = data["runtime"]
-genre = data["genres"][0]["name"]
-content_rating = data["releases"]["countries"][0]["certification"]
-rating = data["vote_average"]
-release_date = data["release_date"]
-
-suicide_squad = media.Movie(title, overview, image, trailer, runtime, genre, content_rating, rating, release_date)
-
-url = "https://api.themoviedb.org/3/movie/37799?api_key=a04cb5827933e112089c0827a6fbf635&append_to_response=videos,releases"
-response = requests.get(url)
-data = json.loads(response.text)
-title = data["original_title"]
-overview = data["overview"]
-image = image_url + data["poster_path"]
-trailer = trailer_url + data["videos"]["results"][0]["key"]
-runtime = data["runtime"]
-genre = data["genres"][0]["name"]
-content_rating = data["releases"]["countries"][0]["certification"]
-rating = data["vote_average"]
-release_date = data["release_date"]
-
-the_social_network = media.Movie(title, overview, image, trailer, runtime, genre, content_rating, rating, release_date) 
-
-url = "https://api.themoviedb.org/3/movie/308639?api_key=a04cb5827933e112089c0827a6fbf635&append_to_response=videos,releases"
-response = requests.get(url)
-data = json.loads(response.text)
-title = data["original_title"]
-overview = data["overview"]
-image = image_url + data["poster_path"]
-trailer = trailer_url + data["videos"]["results"][0]["key"]
-runtime = data["runtime"]
-genre = data["genres"][0]["name"]
-content_rating = data["releases"]["countries"][0]["certification"]
-rating = data["vote_average"]
-release_date = data["release_date"]
-
-dope = media.Movie(title, overview, image, trailer, runtime, genre, content_rating, rating, release_date)
-
-url = "https://api.themoviedb.org/3/movie/77016?api_key=a04cb5827933e112089c0827a6fbf635&append_to_response=videos,releases"
-response = requests.get(url)
-data = json.loads(response.text)
-title = data["original_title"]
-overview = data["overview"]
-image = image_url + data["poster_path"]
-trailer = trailer_url + data["videos"]["results"][0]["key"]
-runtime = data["runtime"]
-genre = data["genres"][0]["name"]
-content_rating = data["releases"]["countries"][0]["certification"]
-rating = data["vote_average"]
-release_date = data["release_date"]
-
-end_of_watch = media.Movie(title, overview, image, trailer, runtime, genre, content_rating, rating, release_date) 
-
+concussion = create_movie(321741)
+why_him = create_movie(356305)
+suicide_squad = create_movie(297761)
+the_social_network = create_movie(37799)
+dope = create_movie(308639)
+end_of_watch = create_movie(77016)
 
 movies = [concussion, why_him, suicide_squad, the_social_network, dope, end_of_watch]
 fresh_tomatoes.open_movies_page(movies)
